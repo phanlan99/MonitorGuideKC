b615861e277ce2cec427342e13e5caeb440425b8
phanlan99 <phanlandt123@gmail.com>
2026-02-26 14:22:13 +0700

    update giờ việt nam

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -2,7 +2,7 @@
 import mysql from 'mysql2/promise';
 
 export const db = mysql.createPool({
-  host: '192.168.122.10', // IP server của bạn
+  host: '192.168.122.10',
   port: 3307,
   user: 'root',
   password: 'Spc123@@@',
@@ -10,4 +10,6 @@ export const db = mysql.createPool({
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
+  timezone: '+07:00', // <--- THÊM: Ép MySQL hiểu query đang gọi từ VN
+  dateStrings: true,  // <--- THÊM: Trả về đúng chuỗi text (14:07) từ DB thay vì ép kiểu Object Date sai lệch
 });
